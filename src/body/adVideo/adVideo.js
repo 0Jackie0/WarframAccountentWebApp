5d60db77261a97b46b438ed2fcdf4f8893ea3cee
Chung Kit Leung (Jackie) <45184415+0Jackie0@users.noreply.github.com>
2020-08-03 12:53:01 -0600

    add aouto replay
    auto replay function to wellcome video

--- src/body/adVideo/adVideo.js
+++ src/body/adVideo/adVideo.js
@@ -4,19 +4,25 @@ import "../../../node_modules/video-react/dist/video-react.css";
 import "./adVideo.css";
 
 // const videoURL = "https://media.w3.org/2010/05/sintel/trailer_hd.mp4";
+const videoURL = "http://192.168.1.52:28590/api/video";
+
 // const videoURL = "http://192.168.1.52:28590/api/video/respond";
-const videoURL = "http://192.168.1.52:28590/api/video/entity";
-// const videoURL = "http://192.168.1.52:28590/api/video/resource";
 
 class AdVideo extends Component
 {
+    replayVideo = ()=>
+    {
+        this.player.seek(0);
+        this.player.play();
+    }
+
     render()
     {
         return (
             <div className="videoArea">
                 <button onClick={this.props.close}>Close</button>
-                <Player ref={player => {this.player = player;}} playsInline autoPlay src={videoURL}>
-                    <ControlBar autoHide={false} />
+                <Player ref={player => {this.player = player;}} playsInline autoPlay src={videoURL} onEnded={this.replayVideo}>
+                    <ControlBar autoHide={true} />
                 </Player>
             </div>
         );
